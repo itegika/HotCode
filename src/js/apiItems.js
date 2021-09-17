72f65276dc09b0164616a269a55722540bd6049c
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-09-17 15:25:54 +0300

    123

--- src/js/apiItems.js
+++ src/js/apiItems.js
@@ -11,8 +11,7 @@ async function fetchTrends(page) {
 document.addEventListener('DOMContentLoaded', fetchTrendsGallery);
 
 async function fetchTrendsGallery(e) {
-
-async function fetchTrends(page) {
+  async function fetchTrends(page) {
     const request = await axios.get(`${URL}/trending/movie/week?api_key=${KEY}&page=${page}`);
     console.log(request);
     const data = request.data;
@@ -22,33 +21,31 @@ async function fetchTrends(page) {
     // console.log(result[0]);
     return result;
   }
-  
-document.addEventListener('DOMContentLoaded', fetchTrendsGallery);
-console.log(document);
-async function fetchTrendsGallery(e) {
-  // e.preventDefault();
 
-  try {
-    const movies = await fetchTrends(1);
-    console.log(movies);
-    renderGallery(movies);
+  document.addEventListener('DOMContentLoaded', fetchTrendsGallery);
+  console.log(document);
+  async function fetchTrendsGallery(e) {
+    // e.preventDefault();
 
-    // console.log(movies);
+    try {
+      const movies = await fetchTrends(1);
+      console.log(movies);
+      renderGallery(movies);
 
-  } catch (error) {
-    console.error(error);
+      // console.log(movies);
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
 
-async function fetchGenres() {
-  try {
-    const res = await axios.get(`${URL}/genre/movie/list?api_key=${KEY}`);
-    const genres = res.data.genres;
-    console.log(genres);
-  } catch (error) {
-    console.error(error);
+  async function fetchGenres() {
+    try {
+      const res = await axios.get(`${URL}/genre/movie/list?api_key=${KEY}`);
+      const genres = res.data.genres;
+      console.log(genres);
+    } catch (error) {
+      console.error(error);
+    }
   }
+  fetchGenres();
 }
-fetchGenres();
-
-
