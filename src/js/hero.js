9ce9c7adf2d8b287c3b1e508def4e69073a967fb
KliffUndersn <83068780+KliffUndersn@users.noreply.github.com>
2021-09-17 17:53:18 +0300

    search works!?!!!!!!

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,14 +1,31 @@
+import axios from 'axios';
+import renderGallery from './cardMaket';
+import debounce from 'lodash.debounce'
+import Notiflix from "notiflix";
+const URL = 'https://api.themoviedb.org/3';
+const KEY = '15f17b74af157d4eeef693405d33f902';
 const refs = {
-    form: document.querySelector('#search-form'),
+  form: document.querySelector('#search-form'),
+  layout__list: document.querySelector('.layout__list'),
+}
+let query = "";
+async function fetchQuery(query) {
+ try {  
+ const request = await axios.get(`${URL}/search/movie?api_key=${KEY}&query=${query}`);  
+ refs.layout__list.innerHTML = "";
+ renderGallery(request.data.results); 
+ if (request.data.results.length === 0) {return error}
+ } catch (error) {
+  Notiflix.Notify.failure(`We can't find this film 😱`)   
+ } 
   }
  
 const searchInput = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-
+  e.preventDefault();
+  query = e.target.value;
+    fetchQuery(query);
   }
   export default searchInput ;
-  refs.form.addEventListener('input', searchInput)
+ refs.form.addEventListener('input', debounce(searchInput,100))
 
 
-  
