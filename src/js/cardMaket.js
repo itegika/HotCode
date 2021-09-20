68e536c895f9c8a577b44b0e0a63bcfd8922c49f
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-09-20 10:32:10 +0300

    updates

--- src/js/cardMaket.js
+++ src/js/cardMaket.js
@@ -1,34 +1,33 @@
-
-import { fetchById } from "./apiItems";
-
+import { fetchById } from './apiItems';
 
 import fetchGenres from './apiItems';
 
-const BASEimgURL ='https://image.tmdb.org/t/p/'
-const SIZE = 'w500'
+const BASEimgURL = 'https://image.tmdb.org/t/p/';
+const SIZE = 'w500';
 const layout__list = document.querySelector('.layout__list');
 console.log(layout__list);
 export default function renderGallery(movies) {
-    console.log(movies);
-      const markup = movies.map((movie => {
-          return `<li class="layout__item">
+  console.log(movies);
+  const markup = movies
+    .map(movie => {
+      return `<li class="layout__item">
 
                       <a class="layout__link" href="#" data-id="${movie.id}">
-                      <img class="layout__image" src="${BASEimgURL}${SIZE}${movie.backdrop_path}" alt="${movie.title}" width="" loading="lazy" />
-
-                      <a class="layout__link" href="${BASEimgURL}${SIZE}${movie.backdrop_path}">
-                      <img class="layout__image" src="${BASEimgURL}${SIZE}${movie.poster_path}" alt="${movie.title}" width="" loading="lazy" />
+                 
+                      <img class="layout__image" src="${BASEimgURL}${SIZE}${
+        movie.poster_path
+      }" alt="${movie.title}" width="" loading="lazy" />
                       </a>
                       <ul class="attribut__list">
                           <li class="attribut__item-title">${movie.original_title}</li>
-                          <li class="attribut__item">${movie.release_date.slice(0,4)}</li>
+                          <li class="attribut__item">${movie.release_date.slice(0, 4)}</li>
                       </ul>
-                  </li>`
-      })).join('');
-        
-          layout__list.insertAdjacentHTML('beforeend', markup);
-          return layout__list;
-      }
+                  </li>`;
+    })
+    .join('');
 
-    //   ${BASEimgURL}${SIZE}${movie.poster_path}
+  layout__list.insertAdjacentHTML('beforeend', markup);
+  return layout__list;
+}
 
+//   ${BASEimgURL}${SIZE}${movie.poster_path}
